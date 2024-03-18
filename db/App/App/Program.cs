2d11e9d8579ab7083c6bf03caf63aeb79d64b742
Pop Norbert <norby1000@yahoo.com>
2024-03-18 07:20:48 +0200

    hw
    hw

---
+++ db/App/App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = "";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            string strStudent = "select * from Students";
+            SqlCommand cmd = new SqlCommand(strStudent, con);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader[0]}, {reader[1]}");
+                }
+            }
+            SqlDataAdapter daStudents = new SqlDataAdapter(strStudent, con);
+            DataSet dset = new DataSet();
+            daStudents.Fill(dset, "Students");
+            foreach (DataRow pRow in dset.Tables["Students"].Rows) {
+                Console.WriteLine($"{pRow["id"]},{pRow["name"]}");
+            }
+            con.Close();
+        }
+    }
+}
